6bd31c8d16dcec06451e4c81bb32e9c91d750ead
Antti Peltonen <antti.peltonen@iki.fi>
2025-09-05 13:24:46 +0300

    ser/deser support for text layer

--- src/layers/text.rs
+++ src/layers/text.rs
@@ -3,10 +3,55 @@
 use crate::layers::Layer;
 use crate::projection::{GeoPos, MapProjection};
 use egui::{Align2, Color32, FontId, Painter, Pos2, Rect, Response};
+use serde::{Deserialize, Serialize};
 use std::any::Any;
 
+/// A helper module for serializing `egui::Color32`.
+mod ser_color {
+    use egui::Color32;
+    use serde::{self, Deserialize, Deserializer, Serializer};
+
+    pub fn serialize<S>(color: &Color32, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let s = color.to_hex();
+        serializer.serialize_str(&s)
+    }
+
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<Color32, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let s = String::deserialize(deserializer)?;
+        if !s.starts_with('#') {
+            return Err(serde::de::Error::custom("hex color must start with '#'"));
+        }
+        let s = &s[1..];
+        let (r, g, b, a) = match s.len() {
+            6 => {
+                let r = u8::from_str_radix(&s[0..2], 16).map_err(serde::de::Error::custom)?;
+                let g = u8::from_str_radix(&s[2..4], 16).map_err(serde::de::Error::custom)?;
+                let b = u8::from_str_radix(&s[4..6], 16).map_err(serde::de::Error::custom)?;
+                (r, g, b, 255)
+            }
+            8 => {
+                let r = u8::from_str_radix(&s[0..2], 16).map_err(serde::de::Error::custom)?;
+                let g = u8::from_str_radix(&s[2..4], 16).map_err(serde::de::Error::custom)?;
+                let b = u8::from_str_radix(&s[4..6], 16).map_err(serde::de::Error::custom)?;
+                let a = u8::from_str_radix(&s[6..8], 16).map_err(serde::de::Error::custom)?;
+                (r, g, b, a)
+            }
+            _ => {
+                return Err(serde::de::Error::custom("invalid hex color length"));
+            }
+        };
+        Ok(Color32::from_rgba_unmultiplied(r, g, b, a))
+    }
+}
+
 /// The size of the text.
-#[derive(Clone, Copy, Debug, PartialEq)]
+#[derive(Clone, Copy, Debug, PartialEq, Serialize, Deserialize)]
 pub enum TextSize {
     /// Size is in screen points, and does not scale with zoom.
     Static(f32),
@@ -16,7 +61,7 @@ pub enum TextSize {
 }
 
 /// A piece of text on the map.
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct Text {
     /// The text to display.
     pub text: String,
@@ -28,9 +73,11 @@ pub struct Text {
     pub size: TextSize,
 
     /// The color of the text.
+    #[serde(with = "ser_color")]
     pub color: Color32,
 
     /// The color of the background.
+    #[serde(with = "ser_color")]
     pub background: Color32,
 }
 
@@ -54,19 +101,24 @@ pub enum TextLayerMode {
 }
 
 /// Layer implementation that allows placing text on the map.
-#[derive(Clone)]
+#[derive(Clone, Serialize, Deserialize)]
+#[serde(default)]
 pub struct TextLayer {
     texts: Vec<Text>,
 
     /// The current mode.
+    #[serde(skip)]
     pub mode: TextLayerMode,
 
     /// The properties for the next text to be added.
+    #[serde(skip)]
     pub new_text_properties: Text,
 
     /// The state of the text currently being edited or added.
+    #[serde(skip)]
     pub editing: Option<EditingText>,
 
+    #[serde(skip)]
     dragged_text_index: Option<usize>,
 }
 
@@ -80,7 +132,7 @@ impl Default for TextLayer {
                 pos: GeoPos { lon: 0.0, lat: 0.0 }, // This will be updated on click.
                 size: TextSize::Static(12.0),
                 color: Color32::BLACK,
-                background: Color32::from_rgba_premultiplied(255, 255, 255, 180),
+                background: Color32::from_rgba_unmultiplied(255, 255, 255, 180),
             },
             editing: None,
             dragged_text_index: None,
@@ -270,5 +322,57 @@ impl TextLayer {
         });
         let screen_pos = projection.project(text.pos);
         Align2::CENTER_CENTER.anchor_rect(Rect::from_min_size(screen_pos, galley.size()))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn text_layer_serde() {
+        let mut layer = TextLayer::default();
+        layer.mode = TextLayerMode::Modify; // This should not be serialized.
+        layer.texts.push(Text {
+            text: "Hello".to_string(),
+            pos: GeoPos { lon: 1.0, lat: 2.0 },
+            size: TextSize::Static(14.0),
+            color: Color32::from_rgb(0, 0, 255),
+            background: Color32::from_rgba_unmultiplied(255, 0, 0, 128),
+        });
+
+        let json = serde_json::to_string(&layer).unwrap();
+
+        // The serialized string should only contain texts.
+        assert!(json.contains(r##""texts":[{"text":"Hello","pos":{"lon":1.0,"lat":2.0},"size":{"Static":14.0},"color":"#0000ffff","background":"#ff000080""##));
+
+        // it should not contain skipped fields
+        assert!(!json.contains("mode"));
+        assert!(!json.contains("new_text_properties"));
+        assert!(!json.contains("editing"));
+        assert!(!json.contains("dragged_text_index"));
+
+        let deserialized: TextLayer = serde_json::from_str(&json).unwrap();
+
+        // Check that texts are restored correctly.
+        assert_eq!(deserialized.texts.len(), 1);
+        assert_eq!(deserialized.texts[0].text, "Hello");
+        assert_eq!(deserialized.texts[0].pos, GeoPos { lon: 1.0, lat: 2.0 });
+        assert_eq!(deserialized.texts[0].size, TextSize::Static(14.0));
+        assert_eq!(deserialized.texts[0].color, Color32::from_rgb(0, 0, 255));
+        assert_eq!(
+            deserialized.texts[0].background,
+            Color32::from_rgba_unmultiplied(255, 0, 0, 128)
+        );
+
+        // Check that skipped fields have their values from the `default()` implementation.
+        let default_layer = TextLayer::default();
+        assert_eq!(deserialized.mode, default_layer.mode);
+        assert_eq!(
+            deserialized.new_text_properties,
+            default_layer.new_text_properties
+        );
+        assert!(deserialized.editing.is_none());
+        assert!(deserialized.dragged_text_index.is_none());
     }
 }
