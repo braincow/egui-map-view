30269eec4f021cf8ff88a8a007b31e6b06b474c7
Antti Peltonen <antti.peltonen@iki.fi>
2025-09-08 11:15:01 +0300

    partial fix only, add_sized works as expected but otherwise we are square

--- src/lib.rs
+++ src/lib.rs
@@ -558,17 +558,8 @@ fn y_to_lat(y: f64, zoom: u8) -> f64 {
 
 impl Widget for &mut Map {
     fn ui(self, ui: &mut Ui) -> Response {
-        // Give it a minimum size so that it does not become too small
-        // in a horizontal layout. Use tile size as minimum.
-        let desired_size = if ui.layout().main_dir().is_horizontal() {
-            // In a horizontal layout, we want to be a square of a reasonable size.
-            let side = TILE_SIZE as f32;
-            Vec2::splat(side)
-        } else {
-            // In a vertical layout, we want to fill the available space.
-            ui.available_size()
-        };
-        let desired_size = desired_size.at_least(Vec2::new(TILE_SIZE as f32, TILE_SIZE as f32));
+        let desired_size = Vec2::splat(TILE_SIZE as f32);
+        let desired_size = desired_size.at_least(Vec2::splat(TILE_SIZE as f32));
 
         let response = ui.allocate_response(desired_size, Sense::drag().union(Sense::click()));
         let rect = response.rect;
