fa8e7c3990d2f0dd52de5632cb1e4fee6a56f802
Antti Peltonen <antti.peltonen@iki.fi>
2025-11-09 10:23:44 +0200

    example with a programtically created circle in draw layer

--- examples/areas.rs
+++ examples/areas.rs
@@ -30,6 +30,8 @@ impl Default for MyApp {
         let mut map = Map::new(OpenStreetMapConfig::default());
         let mut area_layer = AreaLayer::default();
         let (center_lon, center_lat) = map.center.into();
+
+        // Add triangle
         area_layer.add_area(Area {
             points: vec![
                 // Create GeoPos points relative to the maps default center
@@ -38,6 +40,24 @@ impl Default for MyApp {
                 (center_lon, center_lat + 1.0).into(),
             ],
         });
+
+        // Add a circle
+        let circle_center_lon = center_lon - 3.5;
+        let circle_center_lat = center_lat;
+        let radius = 1.0;
+        let num_points = 64;
+        let mut circle_points = Vec::with_capacity(num_points);
+        for i in 0..num_points {
+            let angle = (i as f64 / num_points as f64) * 2.0 * std::f64::consts::PI;
+            let lon =
+                circle_center_lon + radius * angle.cos() / circle_center_lat.to_radians().cos();
+            let lat = circle_center_lat + radius * angle.sin();
+            circle_points.push((lon, lat).into());
+        }
+        area_layer.add_area(Area {
+            points: circle_points,
+        });
+
         map.add_layer("areas", area_layer);
         Self { map }
     }
@@ -62,15 +82,6 @@ impl eframe::App for MyApp {
                         ui.radio_value(&mut area_layer.mode, AreaMode::Disabled, "Disabled");
                         ui.radio_value(&mut area_layer.mode, AreaMode::Modify, "Modify");
                     });
-
-                    // ui.add(
-                    //     egui::Slider::new(&mut drawing_layer.stroke.width, 0.1..=10.0)
-                    //         .text("Stroke width"),
-                    // );
-                    // ui.horizontal(|ui| {
-                    //     ui.label("Stroke color:");
-                    //     ui.color_edit_button_srgba(&mut drawing_layer.stroke.color);
-                    // });
                 }
             });
     }
